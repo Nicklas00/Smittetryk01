d8b05dbd64bbc3b25659338634d7b73cff51d45d
ERLMKea <erlm@kea.dk>
2022-02-04 20:46:24 +0100

    RestController

---
+++ src/main/java/com/example/smittetryk01/controller/CountyRESTController.java
@@ -0,0 +1,24 @@
+package com.example.smittetryk01.controller;
+
+import com.example.smittetryk01.model.County;
+import com.example.smittetryk01.repository.CountyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountyRESTController {
+
+    @Autowired
+    CountyRepository countyRepository;
+
+    @GetMapping("/counties")
+    public List<County> getAllCounties() {
+        return countyRepository.findAll();
+
+    }
+}
